0f6043da12d762a77ec5324ee844ee9f2f7706cc
AC287 <arthurchen287@gmail.com>
2016-02-26 13:29:30 -0500

    created based file

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var express = require('express');
+var morgan = requir('morgan');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var path = require('path');
+var methodOverride = require('method-override');
